83bc1757f49e5c0e82b16dbceb9d5c4a9465c119
Mark <mark@rendlelabs.com>
2023-02-28 17:05:22 +0000

    Fixing workflow

--- src/dotnet-detect-changes/PathHelper.cs
+++ src/dotnet-detect-changes/PathHelper.cs
@@ -2,6 +2,11 @@
 
 public static class PathHelper
 {
+    /// <summary>
+    /// Fixes directory separator characters in paths
+    /// </summary>
+    /// <param name="original">The original string</param>
+    /// <returns>Fixed string</returns>
     public static string FixSeparator(string original)
     {
         return Path.DirectorySeparatorChar switch
